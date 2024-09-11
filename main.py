0009265424c85395c70928f0a2617599c0031385
BoyerCoding <BoyerCoding@gmail.com>
2024-09-11 11:48:12 -0400

    Added content

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 import streamlit as st
 
-# Set up the page configuration
+# Set up the pages configuration
 st.set_page_config(layout="wide", page_icon="Images/Icon.webp", page_title="Graeden Boyer")
 
-# Data for the rows
-data = [
-    {"title": "Todo app", "image": "Images/TodoApp.png", "description": "This is my todo app. I used figma for planning out the design then used streamlit as the main functionality of the program"},
-    {"title": "Camera Converter", "image": "Images/TodoApp.png", "description": "Description"}
-]
+# Create two columns with the specified gap
+col1, col2 = st.columns(2, gap="small")
 
-# Create rows and display content in columns
-for i, item in enumerate(data):
-    # Create two columns with the specified gap
-    col1, col2 = st.columns(2, gap="small")
+# Add content to the first column
+with col1:
+    st.image("Images/photo.png",use_column_width="always")
 
-    # Add content to the first column
-    with col1:
-        st.title(item["title"])
-        st.image(item["image"])
+# Add content to the second column
+with col2:
+    st.title("Graeden Boyer")
+    content = '''Hey, I am Graeden! I have been studying technologies hoping to land my first job in tech. I have been working in Sales for the last 5 years out performing everyone in our market.
+    I have always had an interest in developing software and becoming a mentor to others in this field. I could never afford the loan to get into college for my field of study therefore,
+    I have taken it upon myself to get the education and experience by developing application and use industry best standards to prepare me for a career in technologies.'''
+    st.write(content)
 
-    # Add content to the second column
-    with col2:
-        st.title(item["description"])
+
+st.write("Below Are some of my projects I have been working on to improve my skills.")
 
 st.session_state

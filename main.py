993d92fb65f066b46d66ef0bf9f8de015c044cef
BoyerCoding <BoyerCoding@gmail.com>
2024-09-11 11:12:13 -0400

    Added Csv File and images

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import streamlit as st
 
-st.set_page_config(layout="wide",page_icon="Images/Icon.webp", page_title="Graeden Boyer")
+# Set up the page configuration
+st.set_page_config(layout="wide", page_icon="Images/Icon.webp", page_title="Graeden Boyer")
 
-data = (({"title":"Todo app", "image": "Images/TodoApp.png"}, {"description": "Description"}),({"title":"Camera Converter", "image": "Images/TodoApp.png"},{"description":"Description"}))
+# Data for the rows
+data = [
+    {"title": "Todo app", "image": "Images/TodoApp.png", "description": "This is my todo app. I used figma for planning out the design then used streamlit as the main functionality of the program"},
+    {"title": "Camera Converter", "image": "Images/TodoApp.png", "description": "Description"}
+]
 
-# change range to change number of rows
-for i, number in enumerate(range(len(data))):
-    # this effects columns/width
-    row = st.columns(2, vertical_alignment="bottom",gap="small")
+# Create rows and display content in columns
+for i, item in enumerate(data):
+    # Create two columns with the specified gap
+    col1, col2 = st.columns(2, gap="small")
 
-    for index, col in enumerate(row):
-        tile = col.container(height= 750, border=False)
-        if index == 0:
-            tile.title(data[i][index]["title"])
-            tile.image(data[i][index]["image"])
-        if index == 1:
-            tile.title(data[i][index]["description"])
+    # Add content to the first column
+    with col1:
+        st.title(item["title"])
+        st.image(item["image"])
+
+    # Add content to the second column
+    with col2:
+        st.title(item["description"])
 
 st.session_state
-
-
-# on creation it should find its data within a csvlist.
-
